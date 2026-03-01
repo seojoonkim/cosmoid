f4001f008df4b07d5333d837a10db17d9a5d0706
Simon Kim <seojoon.kim@gmail.com>
2026-03-02 00:25:11 +0900

    style: Hero 슬라이드 전환 0.9s + 5초 간격으로 더 천천히

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,7 @@ function TypingH1() {
   useEffect(() => {
     const id = setInterval(() => {
       setCaseIdx(c => (c + 1) % H1_CASES.length);
-    }, 3200);
+    }, 5000);
     return () => clearInterval(id);
   }, []);
 
@@ -36,7 +36,7 @@ function TypingH1() {
             initial={{ y: "60%", opacity: 0 }}
             animate={{ y: "0%", opacity: 1 }}
             exit={{ y: "-60%", opacity: 0 }}
-            transition={{ duration: 0.45, ease: [0.22, 1, 0.36, 1] }}
+            transition={{ duration: 0.9, ease: [0.16, 1, 0.3, 1] }}
           >
             <span style={{display:"block", color:"white"}}>{H1_CASES[caseIdx][0]}</span>
             <span style={{display:"block", color:"white"}}>{H1_CASES[caseIdx][1]}</span>
